cfaf8c5ce36b77e5a72de00638879020b90dd089
Jumas-Cola <kbbyfl91@gmail.com>
2018-08-22 07:54:07 +0300

    added downl_from_group_vk.py

---
+++ downl_from_group_vk/README.md
@@ -0,0 +1,24 @@
+downl_from_group_vk ![Python 3.6](https://pp.userapi.com/c846523/v846523407/b716d/N3RXKWFcPS0.jpg)
+======
+**downl_from_group_vk** – скрипт для скачивания файлов со стены для социальной сети Вконтакте (vk.com)
+
+Download only photos and docs
+------------
+
+Usage
+------------
+```shell
+python3 downl_from_group_vk.py <login> <password> <owner> <count> <offset> <-sep> <-nd>
+```
+Necessary
+------------
+* <***login***>
+* <***password***>
+
+Optional
+------------
+* <***owner***> - owner=123456789(for user) OR owner=-123456789(for group) [default: your own wall]
+* <***count***> - count of 100-s(if count=5 : posts=500). If the number of posts is not a multiple of 100 - superfluous will be ignored. [default: 1 (100 posts)]
+* <***offset***> - offset from newest post [default: 0]
+* <***-sep***> - files to separate folders [default: all in one folder]
+* <***-nd***> - not download. Only .txt file with links. [default: download]
---
+++ downl_from_group_vk/downl_from_group_vk.py
@@ -0,0 +1,134 @@
+from urllib.parse import urlparse
+from copy import copy
+import urllib.request
+import inspect
+import random
+import vk_api
+import time
+import sys
+import os
+
+
+def get_script_dir(follow_symlinks=True):
+    if getattr(sys, 'frozen', False): # py2exe, PyInstaller, cx_Freeze
+        path = os.path.abspath(sys.executable)
+    else:
+        path = inspect.getabsfile(get_script_dir)
+    if follow_symlinks:
+        path = os.path.realpath(path)
+    return os.path.dirname(path)
+
+def file_write(string, file_name=get_script_dir()+"/"+'files_vk.txt'):
+	f = open(file_name,'a')
+	f.write(str(string))
+	f.write("\n")
+	f.close()
+
+def downloader(url, num, file, downl_try_count):
+	downl_try = 0
+	while downl_try != downl_try_count:
+		try:
+			urllib.request.urlretrieve(url, "{}/{}/{}".format(get_script_dir(),str(num),file))
+			break
+		except:
+			downl_try+=1
+	if downl_try == downl_try_count:
+		print("Download failed")
+		file_write("Download failed")
+
+"""
+login = '+79168450314'
+password = 'X-dcl6q6t7G@'
+# id владельца стены(в группах id с минусом)
+owner = -168416289
+"""
+if len(sys.argv) <3:
+	print("--------------------------------------------------\n")
+	print("Usage: downl_from_group_vk.py <login> <password> <owner> <count> <offset> <-sep> <-nd>\n")
+	sys.exit(1)
+
+
+try:
+	login = sys.argv[1]
+	password = sys.argv[2]
+except:
+	sys.exit(1)
+try:
+	owner = sys.argv[3]
+except:
+	owner = ''
+try:
+	count = int(sys.argv[4])
+except:
+	count = 1
+try:
+	offset = int(sys.argv[5])
+except:
+	offset = 0
+try:
+	if sys.argv[6]=='-sep':
+		separate = True
+except:
+	separate = False
+try:
+	if sys.argv[7]=='-nd':
+		download = False
+except:
+	download = True
+
+
+downl_try_count = 50
+
+
+print("--------------------------------------------------\n")
+print("Owner: {}\n".format(owner))
+print("Count of 100-s: {}\n".format(str(count)))
+print("Offset: {}\n".format(str(offset)))
+print("Separate to folders: {}\n".format(str(separate)))
+print("Need to download: {}\n".format(str(download)))
+
+
+vk = vk_api.VkApi(login=login, password=password)
+vk.auth()
+photo_sizes = ['photo_2560', 'photo_1280', 'photo_807', 'photo_604', 'photo_130', 'photo_75']
+num = 0
+
+for i in range(count):
+	posts = vk.method('wall.get', {'owner_id': owner, 'count': 100, 'filter': 'owner', 'offset': offset})['items']
+	for post in posts:
+		if 'attachments' in post:
+			for attachment in post['attachments']:
+				if attachment['type'] == 'photo':
+					photo = attachment['photo']
+					for size in photo_sizes:
+						if size in photo:
+							url = photo[size]
+							file = urlparse(photo[size])[2].split('/')[-1]
+							try:
+								os.makedirs(get_script_dir()+"/"+str(num))
+							except:
+								pass
+							print(url)
+							file_write(str(num)+"\t"+url)
+							if download:
+								downloader(url, num, file, downl_try_count)
+							break
+				if attachment['type'] == 'doc':
+					doc = attachment['doc']
+					url = doc['url']
+					file = doc['title']
+					try:
+						os.makedirs(get_script_dir()+"/"+str(num))
+					except:
+						pass
+					print(url)
+					file_write(str(num)+"\t"+url)
+					if download:
+						downloader(url, num, file, downl_try_count)
+
+		if separate:
+			num += 1
+	time.sleep(1)
+	offset+=100
+print("--------------------------------------------------\n")
+print("Finished!\n")
