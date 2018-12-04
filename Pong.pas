1f4c2962c912df740c756d095196a05dfb0db67c
Jumas-Cola <kbbyfl91@gmail.com>
2018-12-04 18:54:57 +0300

    fixed bugs

--- Pong.pas
+++ Pong.pas
@@ -52,7 +52,7 @@ begin
 end;
 
 // автоматическая игра левой ракетки
-procedure auto_play(start_vel:integer);
+procedure auto_play_l(start_vel:integer);
 begin
 var lp_y_center := lp.y + trunc(lp.size_y/2);
 if b.y <> lp_y_center then
@@ -62,6 +62,20 @@ if b.y <> lp_y_center then
     else
       lp.vel := sign(b.y - lp_y_center) * start_vel;
     lp.y += lp.vel;
+  end;
+end;
+
+// автоматическая игра правой ракетки
+procedure auto_play_r(start_vel:integer);
+begin
+var rp_y_center := rp.y + trunc(rp.size_y/2);
+if b.y <> rp_y_center then
+  begin
+    if abs(b.y - rp_y_center) <= start_vel then
+      rp.vel := sign(b.y - rp_y_center)
+    else
+      rp.vel := sign(b.y - rp_y_center) * start_vel;
+    rp.y += rp.vel;
   end;
 end;
 
@@ -94,7 +108,7 @@ brush.Color := clBlack;
 while True do
   begin
     // респаун мячика
-    if (b.x+rp.size_x+b.rad>WindowWidth) then
+    if (b.x > WindowWidth) then
       begin
         b.x := trunc(WindowWidth/2); 
         b.y := trunc(WindowHeight/2);
@@ -102,7 +116,7 @@ while True do
         b.vel_y := random(rand_y)-trunc(rand_y/2);
         l_goals+=1;
       end
-    else if (b.x-rp.size_x-b.rad<0) then
+    else if (b.x < 0) then
       begin
         b.x := trunc(WindowWidth/2); 
         b.y := trunc(WindowHeight/2);
@@ -111,17 +125,19 @@ while True do
         r_goals+=1;
       end;
     // отскок от верхней и нижней стенки
-    if ((b.y+b.rad)>WindowHeight) or ((b.y-b.rad)<0) then
+    if ((b.y+b.rad)>=WindowHeight) or ((b.y-b.rad)<=0) then
       b.vel_y:=-b.vel_y;
     // отскок от правой ракетки
-    if (b.x in [rp.x-b.rad..rp.x+rp.size_x+b.rad]) and (b.y in [rp.y..rp.y+rp.size_y]) then
+    if (b.x > (WindowWidth-rp.size_x-b.rad)) and (b.y in [rp.y..rp.y+rp.size_y]) then
       begin
+        b.x := WindowWidth-rp.size_x-b.rad;
         b.vel_x:=-b.vel_x;
         b.vel_y+=random(rand_y)-trunc(rand_y/2);
       end;
     // отскок от левой ракетки
-    if (b.x in [lp.x-b.rad..lp.x+lp.size_x+b.rad]) and (b.y in [lp.y..lp.y+lp.size_y]) then
+    if (b.x < (lp.size_x+b.rad)) and (b.y in [lp.y..lp.y+lp.size_y]) then
       begin
+        b.x := lp.size_x+b.rad;
         b.vel_x:=-b.vel_x;
         b.vel_y+=random(rand_y)-trunc(rand_y/2);
       end;
@@ -141,14 +157,15 @@ while True do
     if l_up then lp.y := lp.y - lp.vel//Up
     else if l_down then lp.y := lp.y + lp.vel;//Down
     
-    auto_play(4);// автоматическая игра левой ракетки
+    auto_play_l(4);// автоматическая игра левой ракетки
+    auto_play_r(8);// автоматическая игра правой ракетки
     
     ClearWindow;
     rp.x := WindowWidth-rp.size_x;
     lp.x := 0;
     SetFontSize(25);
     // отрисовка счёта
-    DrawTextCentered(0, 0, WindowWidth, trunc(WindowHeight/3),l_goals+':'+r_goals);
+    DrawTextCentered(0, 0, WindowWidth, trunc(WindowHeight/3),l_goals+'     '+r_goals);
     // отрисовка разметки поля
     Line(trunc(WindowWidth/2), 0, trunc(WindowWidth/2), WindowHeight);
     DrawCircle(trunc(WindowWidth/2), trunc(WindowHeight/2), trunc(WindowWidth/5));
