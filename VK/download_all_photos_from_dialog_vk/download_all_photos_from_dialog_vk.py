aa1582fabed1e3ee00376984385199252cac706f
starlightx <kbbyfl91@gmail.com>
2019-10-05 20:44:51 +0300

    changed dialog downloader

--- VK/download_all_photos_from_dialog_vk/download_all_photos_from_dialog_vk.py
+++ VK/download_all_photos_from_dialog_vk/download_all_photos_from_dialog_vk.py
@@ -5,63 +5,58 @@ import os
 
 
 def download(url, path):
-	file = '_'.join(urlparse(photo[size])[2].split('/')[-4:])
-	urllib.request.urlretrieve(url, path+'/'+file)
+    file = '_'.join(urlparse(photo[size])[2].split('/')[-4:])
+    urllib.request.urlretrieve(url, path + '/' + file)
+
 
 def file_write(file_name='New_File', string=''):
-	f = open(file_name,'a')
-	f.write(string)
-	f.write("\n")
-	f.close()
-
-
-
+    f = open(file_name, 'a')
+    f.write(string)
+    f.write("\n")
+    f.close()
 
 
 # id диалога (параметр sel)
 peer_id = 123456789
-# тип медиа (в данном случае - фотографии)
+token = ''
+
+
+vk = vk_api.VkApi(token=token)
 media_type = 'photo'
-# имя папки, куда скачивать фото (создаётся самой программой)
-name = 'New_Folder'
-
-login = 'login'
-
-password = 'password'
-
-
-
-
-
-photo_sizes = ['photo_2560', 'photo_1280', 'photo_807', 'photo_604', 'photo_130', 'photo_75']
-
-vk = vk_api.VkApi(login=login, password=password)
-
-vk.auth()
+user_id = vk.method('users.get')[0]['id']
+photo_sizes = ['photo_2560', 'photo_1280',
+               'photo_807', 'photo_604', 'photo_130', 'photo_75']
+name = 'user_{}_dialog_{}_dump'.format(user_id, peer_id)
 
 try:
-	os.makedirs(name)
+    os.makedirs(name)
 except:
-	pass
+    pass
 
-messages_with_attachments = vk.method('messages.getHistoryAttachments', {'peer_id': peer_id, 'media_type': media_type, 'count': 200, 'v':'5.62'})
+messages_with_attachments = vk.method('messages.getHistoryAttachments', {
+                                      'peer_id': peer_id, 'media_type': media_type, 'count': 200, 'v': '5.62'})
 next_from = messages_with_attachments['next_from']
 
+urls = []
 
 while next_from:
-	for message_with_attachments in messages_with_attachments['items']:
-		attachment = message_with_attachments['attachment']
-		media = attachment[media_type]
-		if media_type == 'photo':
-			photo = media
-			for size in photo_sizes:
-				if size in photo:
-					url = photo[size]
-					file_write(name+'.txt', url)
-					download(url, name)
-					break
-	messages_with_attachments = vk.method('messages.getHistoryAttachments', {'peer_id': peer_id, 'media_type': media_type, 'start_from': next_from, 'count': 200})
-	try:
-		next_from = messages_with_attachments['next_from']
-	except:
-		next_from = ''
+    print(next_from)
+    for message_with_attachments in messages_with_attachments['items']:
+        attachment = message_with_attachments['attachment']
+        media = attachment[media_type]
+        if media_type == 'photo':
+            photo = media
+            for size in photo_sizes:
+                if size in photo:
+                    url = photo[size]
+                    if url not in urls:
+                        file_write(name + '.txt', url)
+                        download(url, name)
+                        urls.append(url)
+                        break
+    messages_with_attachments = vk.method('messages.getHistoryAttachments', {
+                                          'peer_id': peer_id, 'media_type': media_type, 'start_from': next_from, 'count': 200, 'v':'5.62'})
+    try:
+        next_from = messages_with_attachments['next_from']
+    except:
+        next_from = ''
