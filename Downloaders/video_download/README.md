05d3aedfee1c7701eff0055e5b242c5f65bfbe3b
Jumas Cola <kbbyfl91@gmail.com>
2020-04-21 07:35:26 +0300

    add video_download

---
+++ Downloaders/video_download/README.md
@@ -0,0 +1,24 @@
+# VIDEO_DOWNLOAD
+
+Tool for downloading video.
+
+## Installation
+
+```bash
+pip3 install -r requirements.txt
+```
+
+## Usage
+
+If autorisation needn't.
+
+```bash
+python3 video_download.py <url1> <url2>...
+#or
+python3 video_download.py -f file.txt
+```
+If need autorisation - put cookies.txt file in script directory.  
+In order to extract cookies from browser use any conforming browser extension for exporting cookies. For example, [cookies.txt](https://chrome.google.com/webstore/detail/cookiestxt/njabckikapfpffapmjgojcnbfjonfjfg) (for Chrome) or [cookies.txt](https://addons.mozilla.org/en-US/firefox/addon/cookies-txt/) (for Firefox)
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ Downloaders/video_download/video_download.py
@@ -0,0 +1,37 @@
+import sys
+import youtube_dl
+import os
+from multiprocessing import Pool
+
+
+def downloader(url):
+
+    print('Start downloading:', url)
+
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': os.path.join('downloads', '%(title)s.%(ext)s'),
+        'nooverwrites': True,
+        'no_warnings': False,
+        'ignoreerrors': True,
+        'cookiefile': os.path.join(os.path.dirname(sys.argv[0]), 'cookies.txt'),
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+
+if __name__ == '__main__':
+
+    if sys.argv[1] == '-f':
+        with open(sys.argv[2]) as f:
+            urls = (url.strip() for url in f.readlines())
+    else:
+        urls = sys.argv[1:]
+
+    if not os.path.isdir('downloads'):
+        os.mkdir('downloads')
+
+    with Pool(5) as p:
+        p.map(downloader, urls)
