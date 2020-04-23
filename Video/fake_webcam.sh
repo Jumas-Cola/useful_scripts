e615af9021154201efbd62f79cb19c9c5e0a3848
Jumas Cola <kbbyfl91@gmail.com>
2020-04-24 02:04:45 +0300

    change fake_webcam

--- Video/fake_webcam.sh
+++ Video/fake_webcam.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+sudo modprobe -r v4l2loopback
+cd "/home/$USER"
+if [ ! -d "/home/$USER/v4l2loopback" ]; then
+    git clone https://github.com/umlaeute/v4l2loopback/
+    cd v4l2loopback
+    make && sudo make install
+fi
+sudo depmod -a
+sudo modprobe videodev
+sudo insmod ./v4l2loopback.ko devices=1 video_nr=2 exclusive_caps=1
 while :; do ffmpeg -re -i "$1" -map 0:v -f v4l2 /dev/video2; done
